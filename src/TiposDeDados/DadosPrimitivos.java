0063c2ff413a2d7bbe1d8cc3d98577d5c849be1d
Mateus Santana <mateusantana@proton.me>
2025-04-25 19:28:05 -0300

    Iniciando o curso batismo em java

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,12 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 18;
+        double altura = 1.66;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999933123213999L;
+    }
+}
